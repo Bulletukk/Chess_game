9cb48ac2c4ca278b28ac2b0174bdb71228ddea33
Bulletukk <benny@tuktuk.no>
2024-07-04 22:00:01 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Chess',
+    version='1.0.0',
+    scripts=['main.py'],
+    description='Chess game with fully implemented GUI and AI',
+    url='https://github.com/Bulletukk/Chess_game',
+    author='Bulletukk',
+    author_email='tootsojahni@anl.gov.kg',
+    license='Laissezfaire',
+    packages=find_packages(),  # Automatically find packages in your directory
+    install_requires=[
+        'random',
+        'enum34',  # Use 'enum34' for compatibility with older Python versions
+        'pygame',
+        'numpy'
+    ],
+)
